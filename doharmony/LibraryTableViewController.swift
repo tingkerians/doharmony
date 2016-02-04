30dd368cc9b447f7e77511a2be084aea40cfc62d
eliakim <eliakim@tingker-mac-server-8gb-2.local>
2016-02-04 16:11:55 +0800

    home GUI status ok side menu ok

--- doharmony/LibraryTableViewController.swift
+++ doharmony/LibraryTableViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class LibraryTableViewController: UITableViewController {
+class LibraryTableViewController: UIViewController {
     
     @IBOutlet weak var menuButton: UIBarButtonItem!
     override func viewDidLoad() {
---
+++ doharmony/sideMenuTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  sideMenuTableViewCell.swift
+//  doharmony
+//
+//  Created by eliakim on 2/4/16.
+//  Copyright © 2016 khemer sone andres. All rights reserved.
+//
+
+import UIKit
+
+class sideMenuTableViewCell: UITableViewCell {
+
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.backgroundColor = UIColor .clearColor()
+        
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+
+}
